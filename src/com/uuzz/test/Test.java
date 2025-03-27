3dbc92ba8134c38b5061bf8d21d26e45fca76ca5
phyxinon <a13456041584@outlook.com>
2025-03-27 16:15:19 +0800

    fix line endings

--- src/com/uuzz/test/Test.java
+++ src/com/uuzz/test/Test.java
@@ -1,4 +1,19 @@
 package com.uuzz.test;
 
+import java.util.Random;
+
 public class Test {
+    public static void main(String[] args){
+        int[] tempArr = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+        Random r = new Random();
+        for (int i = 0;i < tempArr.length;i++){
+            int index = r.nextInt(tempArr.length);
+            int temp = tempArr[i];
+            tempArr[i] = tempArr[index];
+            tempArr[index] = temp;
+        }
+        for (int i = 0;i < tempArr.length;i++){
+            System.out.println(tempArr[i]);
+        }
+    }
 }
